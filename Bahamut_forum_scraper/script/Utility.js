9b2214ecdf897a2e950c05d431758832ccfabce7
Calvin Chen <calvin.chen@xperi.com>
2023-08-07 18:05:17 +0800

    Clear files

--- Bahamut_forum_scraper/script/Utility.js
+++
@@ -1,6 +0,0 @@
-let UpdateKeywordUI = function(disabled=false) {
-    let inputKeyword = document.getElementById('idKeyword');
-    let idBtnSearchKeyword = document.getElementById('idBtnSearchKeyword');
-    inputKeyword.disabled = disabled;
-    idBtnSearchKeyword.disabled = disabled;
-};
